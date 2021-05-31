239bef4ad2561902751273fd36c572b2062e3f2f
Kinshuk Phalke <phalkekinshuk@gmail.com>
2021-05-31 14:17:33 +0400

    Completed Spam Classifer with TF-IDF, Stemming, Tokenizing and Stop Words used Count Vectorization

--- Spam Classifier.py
+++ Spam Classifier.py
@@ -1,50 +1,60 @@
-import nltk
 from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 import matplotlib.pyplot as plt
-from math import log, sqrt
 import pandas as pd
 import numpy as np
 from wordcloud import WordCloud
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import accuracy_score
 
 
-def process_message(message, lower_case=True, stem=True, stop_words=True, gram=2):
-    if lower_case:
-        message = message.lower()
-    words = word_tokenize(message)
-    words = [w for w in words if len(w) > 2]
-    if gram > 1:
-        w = []
-        for i in range(len(words) - gram + 1):
-            w += [' '.join(words[i:i + gram])]
-        return w
-    if stop_words:
-        sw=stopwords.words('english')
-        words=[word for word in words if word not in sw]
-    if stem:
-        words = [PorterStemmer().stem(word) for word in words]
-    return words
+class StemmedCountVectorizer(CountVectorizer):
+    def build_analyzer(self):
+        analyzer = super(StemmedCountVectorizer, self).build_analyzer()
+        return lambda doc: ([stemmer.stem(w) for w in analyzer(doc)])
 
 
-def word_graph(totalMails):
+def split_data(mails, totalMails):
     trainIndex, testIndex = list(), list()
     for i in range(1, totalMails):
         if np.random.uniform(0, 1) < 0.75:
             trainIndex += [i]
-    else:
-        testIndex += [i]
-    trainData = mails.loc[trainIndex]
-    testData = mails.loc[testIndex]
-    spam_words = ' '.join(list(mails[mails['label'] == 'spam']['messages']))
+        else:
+            testIndex += [i]
+    trainData = mails.iloc[trainIndex]
+    testData = mails.iloc[testIndex]
+    return trainData, testData
+
+
+def word_graph(mails, val):
+    spam_words = ' '.join(list(mails[mails['class'] == val]['messages']))
     spam_wc = WordCloud(width=512, height=512).generate(spam_words)
     plt.figure(figsize=(10, 8), facecolor='k')
     plt.imshow(spam_wc)
     plt.tight_layout(pad=0)
     plt.show()
 
+
+stemmer = PorterStemmer()
 mails = pd.read_csv('.\input\spam.csv', encoding="latin-1")
 mails.rename({'v1': 'label', 'v2': 'messages'}, axis=1, inplace=True)
 mails.drop(mails.filter(regex="Unname"), axis=1, inplace=True)
 totalMails = mails['messages'].shape[0]
-print(mails['messages'].head().apply(process_message))
+mails['class'] = mails['label'].map({'ham': 0, 'spam': 1})
+mails.drop(['label'], axis=1, inplace=True)
+[trainData, testData] = split_data(mails, totalMails)
+# word_graph(mails, 1) #displays the word graph for spam
+# word_graph(mails,0) #displays the word graph for ham
+model = Pipeline([('vect', StemmedCountVectorizer(min_df=3, stop_words='english', ngram_range=(1, 2), tokenizer=word_tokenize, strip_accents='unicode')),
+                  ('tfidf', TfidfTransformer()),
+                  ('clf', LogisticRegression())])
+model.fit(trainData['messages'], trainData['class'])
+predicted = model.predict(testData['messages'])
+print('Accuracy:', accuracy_score(testData['class'], predicted) * 100, "%")
+# c1 = ['Congratulations ur awarded free $500'] prints 1
+# content = pd.DataFrame(c1, columns=['Test Data'])
+# print(model.predict(content['Test Data']))
